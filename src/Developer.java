43f12d3e623f8f6c184aac064f1d3aad5a330343
VincentFe <vincent.ferrante@student.kuleuven.be>
2023-03-07 00:10:48 +0100

    Initial commit of extended user classes, still some docu needed

--- src/Developer.java
+++ src/Developer.java
@@ -2,8 +2,18 @@ package src;
 
 public class Developer extends User {
 
+	/**
+	 * The constructor instantiates the super class (User) with the correct name.
+	 * @param name is the name of the user.
+	 */
 	public Developer(String name) {
 		super.name = name;
 	}
 
+	//public void setStatus(Task task, Status status) {
+	//	if (status.isAvailable() && (status.makeString() != "executing" || status.makeString() != "finished" || status.makeString() != "failed" )) {
+	//		throw new IllegalArgumentException();
+	//	}
+	//	task.setStatus(status);
+	//}
 }
--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -4,19 +4,34 @@ import java.util.List;
 
 public class ProjectManager extends User {
 	
+	/**
+	 * Contains the list of all projects this user is managing.
+	 */
 	protected List<Project> projects;
 
+	/**
+	 * The constructor instantiates the super class (User) with the correct name.
+	 * @param name is the name of the user.
+	 */
 	public ProjectManager(String name) {
 		super.name = name;
 	}
 
-	
+	// USE CASE:CREATE TASK
+	/**
+	 * This function will update the given project; It will get the necessary info of the failed task and update the new
+	 * 	one accordingly.
+	 * @param project is the project that must be updated
+	 * @param failedTask is the task that has failed and therefore must be updated
+	 * @param newTask is the task that must replace the failed task in the given project.
+	 */
 	public void replaceTask (Project project, Task failedTask, Task newTask) {
-		if (failedTask.getStatus().getStatus() !=  "failed") { // || !this.projects.contains(failedTask)
-			// Throw new error
+		if (failedTask.getStatus().makeString() !=  "failed") { // || !this.projects.contains(failedTask)
+			throw new IllegalArgumentException();
 		}
 		else {
-			failedTask.replace(newTask);
+			String timeSpent = failedTask.spentTime();
+			newTask.updateTime(timeSpent);
 			project.replace(failedTask, newTask);
 			
 			// This alternative task replaces the failed task with respect to
