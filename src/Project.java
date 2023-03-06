562103f9704577e26fa18193faf42006cc3e5cd0
VincentFe <vincent.ferrante@student.kuleuven.be>
2023-03-06 23:21:21 +0100

    Should be ready

--- src/Project.java
+++ src/Project.java
@@ -3,41 +3,72 @@ package src;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-//THIS CLASS IS NOT UP TO DATE YET
+import java.util.Map;
 
 public class Project {
 	private HashMap<Task,List<Task>> dependencies; 
 	private String name;
 	private String description;
-	private int creationTime;
-	private int dueTime;
+	private String dueTime;
 	private List<Task> tasks;
+	private Status status;
+	private long creationTime;
 
-	public Project(String name, String desc, int dueTime) {
+	/**
+	 * @param The project will be instantiated with the given name.
+	 * @param The project will be instantiated with the given description.
+	 * @param The project will be instantiated with the given due time.
+	 * @post A status class will be created to represent this project
+	 * @post The time of creation will be instantiated with the time now
+	 */
+	public Project(String name, String desc, String dueTime) {
 		this.name = name;
 		this.description = desc;
 		this.dueTime = dueTime;
-		//this.creationTime = System.currentTimeMillis();
+		this.status = new Status();
+		this.creationTime = System.currentTimeMillis();
 	};
 	
+	/**
+	 * Checks every task associated with this project; if one isn't marked as finished the project can't be
+	 * 	finished so returns false; if every task is finished the project will be finished and this will 
+	 * 	return true.
+	 */
 	public boolean isFinished() {
 		for (Task t : this.tasks) {
-			if ( t.getStatus().getStatus() != "finished") {return false;}
+			if ( !t.isFinished()) {return false;}
 		}
-		return false;
+		return true;
 	}
 	
+	/**
+	 * this function adds a task and it's dependencies to the Map of all tasks & their dependencies. The function 
+	 * 	checkCycles is called to check if there are cycles in the updated 'graph'; if not then the 'graph' will be updated
+	 * 	and the function will return true, but of there is a cycle, the Map will not be updated and the function will 
+	 *  return false.
+	 * @param task is the new task
+	 * @param dependsOn are the dependencies of the new (previous parameter) task
+	 */
 	public boolean addTask(Task task, List<Task> dependsOn) { 
 		HashMap<Task,List<Task>> newDependencies = (HashMap<Task,List<Task>>) this.dependencies.clone();
 		newDependencies.put(task, dependsOn);
 		if (!checkCycles(task, newDependencies)) {return false;}
 		else {
 			this.dependencies = newDependencies;
+			this.tasks.add(task);
 			return true;
 		}
 	}
 	
+	/**
+	 * THIS FUNCTION DOESN'T WORK AS WANTED -> TO IMPLEMENT WITH BACKTRACKING OR DFS (?)
+	 * 
+	 * This function checks if there are cycles in the dependency 'graph'. It will start by checking the new task (the one 
+	 * 	added in addTask) and will check for a cycle by keeping a list with all encountered tasks and checking if a new task 
+	 * 	already is in this list.
+	 * @param task
+	 * @param dependencies
+	 */
 	private boolean checkCycles(Task task, HashMap<Task,List<Task>> dependencies) {
 		ArrayList<Task> allTasks = new ArrayList<Task>();
 		allTasks.add(task);
@@ -48,16 +79,41 @@ public class Project {
 				else {return false;}
 			}					
 		}
-		return false;		
+		return true;		
 	}
 	
-	public List<Task> getDependencies(Task task) {
-		return this.dependencies.get(task);
-	}
-	
+	/**
+	 * This function replaces the dependencies from old to new; In both the dependencies where the old task is depenendent
+	 * 	as where the old task is the dependant.
+	 * @param oldTask is the task that was marked as failed
+	 * @param newTask is the task that replaces the failed task
+	 */
 	public void replace(Task oldTask, Task newTask) {
 		List <Task> dep = this.dependencies.get(oldTask);
-		this.dependencies.remove(oldTask);
+		this.dependencies.remove(oldTask,dep);
 		this.dependencies.put(newTask, dep);
+		for (Map.Entry<Task, List<Task>> entry : this.dependencies.entrySet()) {
+			List<Task> tempList = entry.getValue();
+			Task tempTask = entry.getKey();
+			if (tempList.contains(oldTask)) {
+				tempList.remove(oldTask);
+				tempList.add(newTask);
+				this.dependencies.put(tempTask, tempList);
+			}
+		}
+	}
+	
+	/**
+	 * This function returns the Tasks that depend on the given task
+	 * @param task is the task that we want the dependants of
+	 */
+	public List<Task> getDependencies(Task task) {
+		List<Task> tasks = new ArrayList<Task>();
+		for (Map.Entry<Task, List<Task>> entry : this.dependencies.entrySet()) {
+			if (entry.getValue().contains(task)) {
+				tasks.add(entry.getKey());
+			}
+		}
+		return tasks;
 	}
 }
