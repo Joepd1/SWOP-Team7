0df61057925d844abe3767cf28fab035160ac417
VincentFe <vincent.ferrante@student.kuleuven.be>
2023-03-06 23:20:33 +0100

    Initial commit Status; To be split so not ready yet

--- src/Status.java
+++ src/Status.java
@@ -1,23 +1,43 @@
 package src;
 
+/**
+ * THIS CLASS NEEDS TO BE SPLIT FOR POLYMORPHISM
+ * 
+ * It also represents if a task is finished, failed, being executed, is waiting for (an)other
+ * 	task(s) to be completed.
+ * @author vince
+ *
+ */
 public class Status {
 
-	private String status;
+	protected String status; // finished, failed, executing, waiting
 	private boolean available;
 	
-	
-	public Status (String status) {
+	/**
+	 * The constructor will be called when a Project or a Task is created. It represents if the task/project is available 
+	 *  for a dev. to execute or not OR.  After creation the task will be available to be executed by a developer.
+	 */
+	public Status () {
 		this.available = true;
-		this.status = "Created";
 	}
 
-	public String getStatus()  {return this.status;}
+	/**
+	 * This function will return the status of the task in String form
+	 */
+	public String makeString()  {return this.status;}
 
+	/**
+	 * This function returns true if the task or project is available for execution, and else in any other case.
+	 */
 	public boolean isAvailable() {return this.available;}
 	
-	public void setStatus(String status) {
-		// Must be a developer
-		
-		this.status = status;
-	}
+	/**
+	 * This function changes the status of this task.
+	 */
+	public void setStatus(String status) {this.status = status;}
+	
+	public void setUnavailable() {this.available = false;}
+	
+	public void setAvailable() {this.available = true;}
+
 }
---
+++ src/TaskStatus.java
@@ -0,0 +1,5 @@
+package src;
+
+public class TaskStatus extends Status {
+
+}
