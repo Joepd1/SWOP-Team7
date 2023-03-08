40aafe8f5709514eb867d92cd073c4a628315a5b
VincentFe <vincent.ferrante@student.kuleuven.be>
2023-03-08 16:24:47 +0100

    TimeSpan defined & documented

--- src/TimeSpan.java
+++ src/TimeSpan.java
@@ -1,31 +1,42 @@
 package src;
 
-
-// TODO The time spans can be used to assess the	estimated time allocated to tasks (to improve future project management).
-
+/**
+ * TODO The time spans can be used to assess the	estimated time allocated to tasks (to improve future project management).
+ * 
+ * @author vincent
+ */
 public class TimeSpan {
+	
+	/**
+	 * @contains the time the associated task was started; i.e. when a developer changed it's status to executing, is null before this.
+	 * @contains the time the associated task was ended; i.e. when a developer changed it's status to finished or failed, is null before this.
+	 * @contains the time that was worked on previous versions of the associated task.
+	 * @contains the timestamp of a finished or failed task; If this is not the case for the associated task this will be null.
+	 * @contains the associated task.
+	 */
 	private long startTime;
 	private long endTime;
 	private long extraTime;
 	private String timeStamp;
+	private Task task;
 
 	/**
-	 * The constructor will start the clock
+	 * The constructor will start the clock and set the the time-collector for replaced tasks to zero.
 	 */
-	public TimeSpan() {
+	public TimeSpan(Task task) {
 		this.startTime = System.currentTimeMillis();
 		this.extraTime = 0;
+		this.task = task;
 	}
 	
-	/**
-	 * @pre task must be finished
+	/* 
+	 * This function will calculate the time that was needed to execute a task and will update the timestamp accordingly.
 	 * 
-	 * This function will calculate the time that was needed to execute a task and will update the timestamp.
+	 * @pre task must be finished or failed
 	 */
 	public void endTask() {
 		this.endTime = System.currentTimeMillis();
-		long elapsedTime = this.endTime - this.startTime;
-		long totalTime = elapsedTime + this.extraTime;
+		long elapsedTime = (this.endTime - this.startTime);
 		int minutes = (int) ((elapsedTime / (1000*60)) % 60);
 		int hours = (int) ((elapsedTime / (1000*60*60)) % 24);
 		String hrs = Integer.toString(hours);
@@ -33,22 +44,24 @@ public class TimeSpan {
 		this.timeStamp = hrs + "h" + mins;
 	}
 
-	/**
-	 * @pre task can't be finished
-	 * 
+	/** 
 	 * This function will calculate the time that was needed until now to try and execute a task and will return the
 	 * 	according timestamp.
+	 * 
+	 * @pre task has to be in executing state
 	 */
 	public String getElapsedTime(Task task) {
-		if (task.getStatus().makeString() != "finished") {
+		if (task.getStatus().makeString() == "executing") {
 			long timeNow = System.currentTimeMillis();
-			long elapsedTime = timeNow - this.startTime;
-			long totalTime = elapsedTime + this.extraTime;
+			long elapsedTime = (timeNow - this.startTime);
 			int minutes = (int) ((elapsedTime / (1000*60)) % 60);
 			int hours = (int) ((elapsedTime / (1000*60*60)) % 24);
 			String hrs = Integer.toString(hours);
 			String mins = Integer.toString(minutes);
 			return hrs + "h" + mins;
+		}
+		else if (task.getStatus().makeString() == "waiting") {
+			throw new IllegalArgumentException();
 		}
 		else {
 			throw new IllegalArgumentException();
@@ -62,10 +75,25 @@ public class TimeSpan {
 	 */
 	public String getTimeSpan() {return this.timeStamp;}
 	
+	
 	/**
-	 * Setter to update the extra time of a new task that replaces another one
-	 * @param time is the time that the replaced task was being executed and thus is added to the extra time that is 
-	 * 	needed to execute the task.
+	 * Getter to calculate the total time worked on this task and possibly replaced tasks
+	 */
+	public String getTotalTime() {
+		String[] temp = this.timeStamp.split("h");
+		long hrs = Integer.parseInt(temp[0]) * 3600000 ;
+		long mins = Integer.parseInt(temp[1]) * 60000;
+		long stamp = hrs + mins + this.extraTime;
+		String hours = Integer.toString((int) ((stamp / (1000*60*60)) % 24));
+		String minutes = Integer.toString((int) ((stamp / (1000*60)) % 60));
+		return hours + "h" + minutes;		
+	}
+	
+	/**
+	 * Setter to update the extra time of a new task that replaces another one.
+	 * 
+	 * @param time is the time that the replaced task was being executed in and thus is added to the extra time that is 
+	 * 	needed to execute the project.
 	 */
 	public void newTaskTime(String time) {
 		String[] arrOfStr = time.split("h");
