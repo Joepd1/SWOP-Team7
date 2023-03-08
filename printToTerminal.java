2f0f6e16d9eccccb9694e23bac6eddab71114653
Joep Demollin <joep.demollin@student.kuleuven.be>
2023-03-08 14:37:34 +0100

    Made function to print to terminal

---
+++ printToTerminal.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+ 
+class GFG {
+    public static void main(String[] args)
+    {
+ 
+        // The cursor will remain
+        // just after the 1
+        System.out.println("GfG1");
+ 
+        // This will be printed
+        // just after the GfG2
+        System.out.println("GfG2");
+    }
+}
